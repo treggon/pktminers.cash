e2dc27ede42b7da2b143d19b42b0df64801025b2
Tregtronics <treggon@gmail.com>
2021-02-16 11:59:33 -0800

    Server script to install docker on centos.

---
+++ server_scripts/setup_docker_centos.sh
@@ -0,0 +1,16 @@
+# Remove docker if it exists
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo docker run hello-world
