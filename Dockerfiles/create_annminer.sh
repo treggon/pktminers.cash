f588f145069c4e579cb2f4584c680a943e6e0f2c
Tregtronics <treggon@gmail.com>
2021-02-16 13:06:50 -0800

    Fixed directory

--- Dockerfiles/create_annminer.sh
+++ Dockerfiles/create_annminer.sh
@@ -1,1 +1,1 @@
-docker build -t treggon/AnnMiner:1.0.0 -t treggon/AnnMiner:latest -f /AnnMiner/Dockerfile .
+docker build -t treggon/AnnMiner:1.0.0 -t treggon/AnnMiner:latest -f AnnMiner/Dockerfile .
