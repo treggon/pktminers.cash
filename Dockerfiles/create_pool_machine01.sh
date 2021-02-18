4f5a28b061b4750e40454d8fec28b6b6d2c6f992
Tregtronics <treggon@gmail.com>
2021-02-17 23:26:14 -0800

    Creating machine 02 script

--- Dockerfiles/create_pool_machine01.sh
+++ Dockerfiles/create_pool_machine01.sh
@@ -1,1 +1,1 @@
-docker build --no-cache -t treggon/pool:1.0.0 -t treggon/pool:latest -f Pool/Machine01/Dockerfile .
+docker build --no-cache -t treggon/pktpoolmachine01:1.0.0 -t treggon/pktpoolmachine01:latest -f Pool/Machine01/Dockerfile .
---
+++ Dockerfiles/create_pool_machine02.sh
@@ -0,0 +1,1 @@
+docker build --no-cache -t treggon/pktpoolmachine02:1.0.0 -t treggon/pktpoolmachine02:latest -f Pool/Machine02/Dockerfile .
