ebeb1979a53c7354c5a3d473fc5e13fc90dd5965
Tregtronics <treggon@gmail.com>
2021-02-17 22:51:45 -0800

    Moving things around for the 2nd machine to work right.

--- Dockerfiles/create_pool.sh
+++ Dockerfiles/create_pool.sh
@@ -1,1 +1,1 @@
-docker build --no-cache -t treggon/pool:1.0.0 -t treggon/pool:latest -f Pool/Dockerfile .
+docker build --no-cache -t treggon/pool:1.0.0 -t treggon/pool:latest -f Pool/Machine01/Dockerfile .
