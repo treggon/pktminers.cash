fc870807f455b8eb6a91c600c7f927c32a4cbcb9
Tregtronics <treggon@gmail.com>
2021-02-18 21:50:40 -0800

    Use Cache now.

--- Dockerfiles/create_pool_machine01.sh
+++ Dockerfiles/create_pool_machine01.sh
@@ -1,1 +1,1 @@
-docker build --no-cache -t treggon/pktpoolmachine01:1.0.0 -t treggon/pktpoolmachine01:latest -f Pool/Machine01/Dockerfile .
+docker build -t treggon/pktpoolmachine01:1.0.0 -t treggon/pktpoolmachine01:latest -f Pool/Machine01/Dockerfile .
