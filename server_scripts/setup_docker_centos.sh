6d86b9f048056cf1e741a8502a0361fe0c14c401
Tregtronics <treggon@gmail.com>
2021-02-22 13:37:07 -0800

    Commit.

--- server_scripts/setup_docker_centos.sh
+++ server_scripts/setup_docker_centos.sh
@@ -11,6 +11,6 @@ sudo yum install -y yum-utils
 sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io
+sudo yum install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo docker run hello-world
