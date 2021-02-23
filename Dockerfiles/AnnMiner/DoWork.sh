1f5e0cb000a37b38fc7ac16471f69f14a4fcc5bd
Tregtronics <treggon@gmail.com>
2021-02-22 20:18:43 -0800

    Go riktor

--- Dockerfiles/AnnMiner/DoWork.sh
+++ Dockerfiles/AnnMiner/DoWork.sh
@@ -5,8 +5,8 @@ do
         echo " About to nuke node"
         pkill -f ./target/release/packetcrypt
         echo " Starting Announcement Miner"
-        nice -n 5 ./target/release/packetcrypt ann http://dalpool01.pktminers.cash/ --paymentaddr pkt1qrfndt6fklnkslkqzwv3gmdmhj9xtatp4gr2479 --threads 32&
+        nice ./target/release/packetcrypt ann http://dalpool01.pktminers.cash/ --paymentaddr pkt1qrfndt6fklnkslkqzwv3gmdmhj9xtatp4gr2479 --threads 35&
         echo " Starting Packet Miner"
-        nice -n 5 ./target/release/packetcrypt blk http://dalpool01.pktminers.cash/ --paymentaddr pkt1qrfndt6fklnkslkqzwv3gmdmhj9xtatp4gr2479 --threads 32&
+        nice ./target/release/packetcrypt blk http://dalpool01.pktminers.cash/ --paymentaddr pkt1qrfndt6fklnkslkqzwv3gmdmhj9xtatp4gr2479 --threads 35&
 sleep ${SLEEP_IN_SECS}
 done
