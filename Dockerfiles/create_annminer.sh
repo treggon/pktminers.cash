e6ea904f78bcdaaf3f52a82bf3934bdf7664001d
Tregtronics <treggon@gmail.com>
2021-02-22 19:14:19 -0800

    Simplifying create_annminer.sh

--- Dockerfiles/create_annminer.sh
+++ Dockerfiles/create_annminer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --no-cache -t treggon/annminer:1.0.0 -t treggon/annminer:latest -f AnnMiner/Dockerfile .
+docker build --no-cache -t treggon/annminer -f AnnMiner/Dockerfile .
