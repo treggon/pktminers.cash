67043ee28363cb7d2468c9dafd8e0c64236e7163
Tregtronics <treggon@gmail.com>
2021-02-23 19:37:23 -0800

    More memory for Block Miners.

--- Dockerfiles/Miner/DoWork.sh
+++ Dockerfiles/Miner/DoWork.sh
@@ -7,6 +7,6 @@ do
         echo " Starting Announcement Miner"
         nice ./target/release/packetcrypt ann http://dalpool01.pktminers.cash/ --paymentaddr pkt1qrfndt6fklnkslkqzwv3gmdmhj9xtatp4gr2479 --threads 35&
         echo " Starting Packet Miner"
-        nice ./target/release/packetcrypt blk http://dalpool01.pktminers.cash/ --paymentaddr pkt1qrfndt6fklnkslkqzwv3gmdmhj9xtatp4gr2479 --threads 35&
+        nice ./target/release/packetcrypt blk http://dalpool01.pktminers.cash/ --memorysizemb 90000 --paymentaddr pkt1qrfndt6fklnkslkqzwv3gmdmhj9xtatp4gr2479 --threads 35&
 sleep ${SLEEP_IN_SECS}
 done
