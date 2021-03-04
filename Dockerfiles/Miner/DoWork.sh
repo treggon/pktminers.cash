71e70f5ca6a0b3146a95461002714e6df492f1ae
Tregtronics <treggon@gmail.com>
2021-03-03 19:58:22 -0800

    Added the pool address to the script.

--- Dockerfiles/Miner/DoWork.sh
+++ Dockerfiles/Miner/DoWork.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-while getopts t:a:b:p: flag
+while getopts t:a:b:p:o: flag
 do
     case "${flag}" in
         t) SLEEP_IN_SECS=${OPTARG};;
         a) annthreads=${OPTARG};;
         b) blockthreads=${OPTARG};;
         p) paymentaddrfixed=${OPTARG};;
+        o) pooladdress=${OPTARG};;
     esac
 done
 
@@ -18,12 +19,12 @@ do
         if [ $annthreads ]
         then
             echo " Starting Announcement Miner"
-            nice ./target/release/packetcrypt ann http://dalpool01.pktminers.cash/ --paymentaddr $paymentaddrfixed --threads $annthreads&
+            nice ./target/release/packetcrypt ann $pooladdress --paymentaddr $paymentaddrfixed --threads $annthreads&
         fi
         if [ $blockthreads ]
         then
             echo " Starting Packet Miner"
-            nice ./target/release/packetcrypt blk http://dalpool01.pktminers.cash/ --memorysizemb 90000 --paymentaddr $paymentaddrfixed --threads $blockthreads&
+            nice ./target/release/packetcrypt blk $pooladdress --memorysizemb 90000 --paymentaddr $paymentaddrfixed --threads $blockthreads&
         fi
 
 sleep ${SLEEP_IN_SECS}
