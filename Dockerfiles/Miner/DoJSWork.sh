609fce008f9cdf9a11723c4bf9af090fe6cb6f51
Tregtronics <treggon@gmail.com>
2021-03-03 20:53:23 -0800

    commit

---
+++ Dockerfiles/Miner/DoJSWork.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+while getopts t:a:b:p:o: flag
+do
+    case "${flag}" in
+        t) SLEEP_IN_SECS=${OPTARG};;
+        a) annthreads=${OPTARG};;
+        b) blockthreads=${OPTARG};;
+        p) paymentaddrfixed=${OPTARG};;
+        o) pooladdress=${OPTARG};;
+        k) maxbw=${OPTARG};;
+        m) maxanns=${OPTARG};;
+    esac
+done
+
+while (( 1 ))
+do
+        echo " About to nuke node"
+        pkill -f ./target/release/packetcrypt
+
+        if [ $annthreads ]
+        then
+            echo " Starting Announcement Miner"
+            node ./PacketCrypt/annmine.js  --maxKbps $maxbw --threads $annthreads --paymentAddr=$paymentaddrfixed $pooladdress &
+        fi
+        if [ $blockthreads ]
+        then
+            echo " Starting Packet Miner"
+            node ./PacketCrypt/blkmine.js --maxKbps $maxbw --threads $annthreads --maxAnns $maxanns --paymentAddr=pRoX4rXvd9JKL8ic4P1X3eDXZkFNk9ai6G $pooladdress &
+        fi
+sleep ${SLEEP_IN_SECS}
+done
