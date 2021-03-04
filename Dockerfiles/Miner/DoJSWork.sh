abfbf57fc42848fafdd906417869c8639a52b92a
Tregtronics <treggon@gmail.com>
2021-03-03 21:57:39 -0800

    go./

--- Dockerfiles/Miner/DoJSWork.sh
+++ Dockerfiles/Miner/DoJSWork.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-while getopts t:a:b:p:o: flag
+while getopts t:a:b:p:h:s:ma: flag
 do
     case "${flag}" in
         t) SLEEP_IN_SECS=${OPTARG};;
         a) annthreads=${OPTARG};;
         b) blockthreads=${OPTARG};;
         p) paymentaddrfixed=${OPTARG};;
-        o) pooladdress=${OPTARG};;
-        k) maxbw=${OPTARG};;
-        m) maxanns=${OPTARG};;
+        h) pooladdress=${OPTARG};;
+        s) maxbw=${OPTARG};;
+        ma) maxanns=${OPTARG};;
     esac
 done
 
@@ -21,12 +21,12 @@ do
         if [ $annthreads ]
         then
             echo " Starting Announcement Miner"
-            node ./annmine.js  --maxKbps $maxbw --threads $annthreads --paymentAddr=$paymentaddrfixed $pooladdress &
+            node ./annmine.js  --maxKbps 10000000 --threads $annthreads --paymentAddr=$paymentaddrfixed $pooladdress &
         fi
         if [ $blockthreads ]
         then
             echo " Starting Packet Miner"
-            node ./blkmine.js --maxKbps $maxbw --threads $annthreads --maxAnns $maxanns --paymentAddr=pRoX4rXvd9JKL8ic4P1X3eDXZkFNk9ai6G $pooladdress &
+            node ./blkmine.js --maxKbps 10000000  --threads $blockthreads --maxAnns 4000000 --paymentAddr=pRoX4rXvd9JKL8ic4P1X3eDXZkFNk9ai6G $pooladdress &
         fi
 sleep ${SLEEP_IN_SECS}
 done
