b9dd08ef330e89f24e3e72957ea508d17d234a04
Tregtronics <treggon@gmail.com>
2021-03-03 21:53:46 -0800

    Fixed

--- Dockerfiles/Miner/DoJSWork.sh
+++ Dockerfiles/Miner/DoJSWork.sh
@@ -21,12 +21,12 @@ do
         if [ $annthreads ]
         then
             echo " Starting Announcement Miner"
-            node ./PacketCrypt/annmine.js  --maxKbps $maxbw --threads $annthreads --paymentAddr=$paymentaddrfixed $pooladdress &
+            node ./annmine.js  --maxKbps $maxbw --threads $annthreads --paymentAddr=$paymentaddrfixed $pooladdress &
         fi
         if [ $blockthreads ]
         then
             echo " Starting Packet Miner"
-            node ./PacketCrypt/blkmine.js --maxKbps $maxbw --threads $annthreads --maxAnns $maxanns --paymentAddr=pRoX4rXvd9JKL8ic4P1X3eDXZkFNk9ai6G $pooladdress &
+            node ./blkmine.js --maxKbps $maxbw --threads $annthreads --maxAnns $maxanns --paymentAddr=pRoX4rXvd9JKL8ic4P1X3eDXZkFNk9ai6G $pooladdress &
         fi
 sleep ${SLEEP_IN_SECS}
 done
