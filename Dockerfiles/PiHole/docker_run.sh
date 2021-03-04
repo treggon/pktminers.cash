265f6931d36e6c0041b55146b9c98eee01d42849
Tregtronics <treggon@gmail.com>
2021-03-03 16:43:57 -0800

    adding host file.

--- Dockerfiles/PiHole/docker_run.sh
+++ Dockerfiles/PiHole/docker_run.sh
@@ -14,7 +14,7 @@ docker run -d \
     -e TZ="America/Chicago" \
     -v "${PIHOLE_BASE}/etc-pihole/:/etc/pihole/" \
     -v "${PIHOLE_BASE}/etc-dnsmasq.d/:/etc/dnsmasq.d/" \
-    --dns=127.0.0.1 --dns=1.1.1.1 \
+    --dns=23.227.170.103 --dns=1.1.1.1 \
     --restart=unless-stopped \
     --hostname pi.hole \
     -e VIRTUAL_HOST="pihole.pktminers.cash" \
