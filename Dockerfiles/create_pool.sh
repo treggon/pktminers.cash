382c7d2278e7245af0227c0d422fd5474cdb7f32
Tregtronics <treggon@gmail.com>
2021-03-04 23:09:13 -0800

    Pool/pool error

--- Dockerfiles/create_pool.sh
+++ Dockerfiles/create_pool.sh
@@ -1,2 +1,2 @@
 [ ! -d "/blockchain/" ] && mkdir /blockchain
-docker build -t treggon/pool:1.0.0 -t treggon/pool:latest -f Pool/Dockerfile .
+docker build -t treggon/pool:1.0.0 -t treggon/pool:latest -f Pool/Pool/Dockerfile .
