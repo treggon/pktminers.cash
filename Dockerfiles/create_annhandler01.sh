6fbf216914f25e8639abbaa0206d575a660be8de
Tregtronics <treggon@gmail.com>
2021-03-15 21:41:44 -0700

    Creating the docker hub names.

--- Dockerfiles/create_annhandler01.sh
+++ Dockerfiles/create_annhandler01.sh
@@ -1,2 +1,2 @@
 [ ! -d "/blockchain/" ] && mkdir /blockchain
-docker build -t tregtronics/pktannh:1.0.0 -t tregtronics/pktannh:latest -f Annhandler/Dockerfile .
+docker build -t tregtronics/pktannh:1.0.0 -t tregtronics/pktannh01:latest -f Annhandler01/Dockerfile .
--- Dockerfiles/create_annhandler02.sh
+++ Dockerfiles/create_annhandler02.sh
@@ -1,2 +1,2 @@
 [ ! -d "/blockchain/" ] && mkdir /blockchain
-docker build -t tregtronics/pktannh:1.0.0 -t tregtronics/pktannh:latest -f Annhandler02/Dockerfile .
+docker build -t tregtronics/pktannh:1.0.0 -t tregtronics/pktannh02:latest -f Annhandler02/Dockerfile .
