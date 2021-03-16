3a514fb4f44fea8c130edee2814d1529f0c7c44a
Tregtronics <treggon@gmail.com>
2021-03-15 22:19:08 -0700

    dns

--- Dockerfiles/create_annhandler01.sh
+++ Dockerfiles/create_annhandler01.sh
@@ -1,2 +1,2 @@
 [ ! -d "/blockchain/" ] && mkdir /blockchain
-docker build -t tregtronics/pktannh:1.0.0 --no-cache -t tregtronics/pktannh01:latest -f Annhandler01/Dockerfile .
+docker build -t tregtronics/pktannh:1.0.0 -t tregtronics/pktannh01:latest -f Annhandler01/Dockerfile .
