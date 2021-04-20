3237502b4e55e7b1c4e0e6da7695c3145b66c1bc
Tregtronics <treggon@gmail.com>
2021-04-20 11:49:39 -0700

    changed thread count for the annhandlers.

--- Dockerfiles/create_pkt.sh
+++ Dockerfiles/create_pkt.sh
@@ -1,2 +1,2 @@
 [ ! -d "/blockchain/" ] && mkdir /blockchain
-docker build --no-cache -t tregtronics/pkt:2.0.0 -t tregtronics/pkt:latest -f Packet/Pool/Dockerfile .
+docker build --no-cache -t tregtronics/pkt:3.0.0 -t tregtronics/pkt:latest -f Packet/Pool/Dockerfile .
